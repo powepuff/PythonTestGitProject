0c28276537075156970a9430710b8875c5001d1d
Danil <powepuff@mail.ru>
2019-04-01 21:57:45 +0300

    Add system info output

--- start.py
+++ start.py
@@ -1,1 +1,9 @@
+import platform
+
 print('PythonTestGitProject')
+
+print(platform.system())
+print(platform.platform())
+print(platform.machine())
+print(platform.version())
+print(platform.processor())
